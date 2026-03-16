3f5b03eb811a7d173e1f917cbbbf1a404b0f5c76
suman jaiswal <51041970+jsonusuman351@users.noreply.github.com>
2026-03-16 13:27:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ def add (a:int, b:int, c:int) -> int:
 def sub(a: int, b: int) -> int:
     return a - b
 
-def mul(a: int, b: int) -> int:
-    return a * b
+def mul(a: int, b: int, c: int) -> int:
+    return a * b *c
 
 def div(a: int, b: int, c:int) -> float:
     return a / b
